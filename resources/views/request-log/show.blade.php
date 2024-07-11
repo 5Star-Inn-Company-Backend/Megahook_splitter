bdabca9a8e4070fc3e67d8f185df60c153a38755
Timtech-wiz <ajetetimothysamuel@gmail.com>
2024-07-11 09:55:37 +0100

    show page

--- resources/views/request-log/show.blade.php
+++ resources/views/request-log/show.blade.php
@@ -7,6 +7,30 @@
         </div>
     </x-slot>
 
+    <div class="max-w-7xl mx-auto p-4 pt-6 pb-8 bg-white rounded shadow-lg">
+  <h1 class="text-3xl font-bold mb-4">Payload Details</h1>
+  <hr>
+  <div class="flex items-center mb-4 pt-4">
+    <span class="text-lg font-semibold mr-2">Bucket:</span>
+    <span class="text-gray-600"> {{$request_log->bucket}} </span>
+  </div>
+  <div class="flex items-center mb-4">
+    <span class="text-lg font-semibold mr-2">Name:</span>
+    <span class="text-gray-600"> {{$request_log->user->name}} </span>
+  </div>
+  <div class="flex items-center mb-4">
+    <span class="text-lg font-semibold mr-2">Destination:</span>
+    <span class="text-gray-600"> {{$request_log->destination}} </span>
+  </div>
+
+  <div class="flex items-center mb-4">
+    <span class="text-lg font-semibold mr-2">Status:</span>
+    <span style="background-color: green;" class="text-white px-4 py-2 text-sm border border-transparent rounded-full"> {{$request_log->status}} </span>
+  </div>
+  <!-- Add more fields as needed -->
+</div>
+
+
      
 
 </x-main-layout>
