47172ae256b201a47e7f50b012e602a62642c644
Timtech-wiz <ajetetimothysamuel@gmail.com>
2025-05-01 08:16:56 +0100

    request log for all destination

--- app/Jobs/SendWebhook.php
+++ app/Jobs/SendWebhook.php
@@ -28,19 +28,27 @@ class SendWebhook implements ShouldQueue
 
     public function handle(): void
     {
-        $response = Http::timeout(5)->post($this->destination->endpoint_url, $this->payload);
-
-        RequestLog::create([
-            'user_id' => $this->webhook->user_id,
-            'bucket' => $this->webhook->input_name,
-            'destination' => $this->destination->destination_name,
-            'status' => $response->successful() ? 'success' : 'failed',
-            'input' => $this->payload,
-            'response_code' => $response->status(),
-        ]);
-
-        if (!$response->successful()) {
-            $this->fail(new Exception('Failed to send webhook payload.'));
+        try {
+            $response = Http::timeout(5)->post($this->destination->endpoint_url, $this->payload);
+    
+            RequestLog::create([
+                'user_id' => $this->webhook->user_id,
+                'bucket' => $this->webhook->input_name,
+                'destination' => $this->destination->destination_name,
+                'status' => $response->successful() ? 'success' : 'failed',
+                'input' => $this->payload,
+                'response_code' => $response->status(),
+            ]);
+    
+        } catch (\Exception $e) {
+             RequestLog::create([
+                'user_id' => $this->webhook->user_id,
+                'bucket' => $this->webhook->input_name,
+                'destination' => $this->destination->destination_name,
+                'status' => 'failed',
+                'input' => $this->payload,
+                'response_code' => $response->status(),
+            ]);
         }
     }
 
